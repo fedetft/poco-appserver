7dafa050cf4fc7bba7477a8ebd6427e6118e12aa
Terraneo Federico <fede.tft@miosix.org>
2014-07-16 09:21:39 +0200

    Example application nearly complete

--- model/file.cpp
+++ model/file.cpp
@@ -1,13 +1,23 @@
 
-#include <iostream>
-#include "application_server.h"
+#include <stdexcept>
 #include "file.h"
 
 using namespace std;
 
-REGISTER_FUNCTION([](){
-    cout<<"Called"<<endl;
-});
+//
+// class UploadedFile
+//
+
+UploadedFile::UploadedFile(const string& clientFileName, const string& mimeType)
+    : clientFileName(clientFileName), mimeType(mimeType)
+{
+    Poco::MD5Engine md5;
+    md5.update(getServerFileName());
+    linkKey=Poco::DigestEngine::digestToHex(md5.digest());
+    
+    deleteTime=chrono::system_clock::now();
+    deleteTime+=chrono::seconds(keepSeconds);
+}
 
 //
 // class FileMap
@@ -19,4 +29,52 @@ FileMap& FileMap::instance()
     return singleton;
 }
 
-FileMap::FileMap() {}
+shared_ptr<UploadedFile> FileMap::add(const string& fileName, const string& mimeType)
+{
+    std::lock_guard<std::mutex> l(m);
+    if(fm.size()>=maxFiles) throw runtime_error("Too many files");
+    shared_ptr<UploadedFile> result(new UploadedFile(fileName,mimeType));
+    fm[result->getLinkKey()]=result;
+    //This is the last created file, must be the last to be deleted
+    filesInDeleteOrder.push_back(result);
+    c.notify_one();
+    return result;
+}
+
+shared_ptr<UploadedFile> FileMap::get(const string& linkKey)
+{
+    std::lock_guard<std::mutex> l(m);
+    return fm[linkKey];
+}
+
+FileMap::FileMap()
+{
+    thread t(&FileMap::fileGarbageCollector,this);
+    t.detach();
+}
+
+void FileMap::fileGarbageCollector()
+{
+    for(;;)
+    {
+        time_point wakeup;
+        {
+            unique_lock<mutex> l(m);
+            while(filesInDeleteOrder.empty()) c.wait(l);
+            wakeup=filesInDeleteOrder.front()->getDeleteTime();
+        }
+        //Wait with the mutex unlocked
+        this_thread::sleep_until(wakeup);
+        {
+            unique_lock<mutex> l(m);
+            //Should never happen but since we've unlocked the mutex...
+            if(filesInDeleteOrder.empty()) continue;
+            //Note that if the file is being downloaded while we remove it
+            //nothing bad happens thanks to the magic of the reference counting,
+            //it will simply be deleted when the download code, that holds the
+            //last reference will end. Easy
+            fm.erase(filesInDeleteOrder.front()->getLinkKey());
+            filesInDeleteOrder.pop_front();
+        }
+    }
+}
--- model/file.h
+++ model/file.h
@@ -3,41 +3,66 @@
 #define FILE_H
 
 #include <map>
+#include <list>
 #include <string>
 #include <utility>
+#include <memory>
+#include <chrono>
+#include <thread>
 #include <mutex>
+#include <condition_variable>
 #include "Poco/TemporaryFile.h"
 #include "Poco/MD5Engine.h"
+
+typedef std::chrono::time_point
+    <std::chrono::system_clock,std::chrono::nanoseconds> time_point;
+
+class UploadedFile
+{
+public:
+    UploadedFile(const std::string& clientFileName,
+                 const std::string& mimeType);
+    
+    std::string getClientFileName() const { return clientFileName; }
+    
+    std::string getServerFileName() const { return file.path(); }
+    
+    std::string getMimeType() const { return mimeType; }
+    
+    std::string getLinkKey() const { return linkKey; }
+    
+    time_point getDeleteTime() const { return deleteTime; }
+    
+    static const unsigned int keepSeconds=300;
+    
+private:
+    std::string clientFileName; ///< Name coming from client
+    std::string mimeType;       ///< Mime type
+    std::string linkKey;        ///< Key used to retrieve file
+    Poco::TemporaryFile file;   ///< Tempfile is deleted by this class dtor
+    time_point deleteTime;      ///< Time when file needs to be deleted
+};
 
 class FileMap
 {
 public:
     static FileMap& instance();
     
-    std::pair<std::string,std::string> add(const std::string& name)
-    {
-        std::lock_guard<std::mutex> l(m);
-        Poco::TemporaryFile temp;
-        temp.keepUntilExit();
-        std::string tempname=temp.path();
-        Poco::MD5Engine md5;
-        md5.update(tempname);
-        std::string key=Poco::DigestEngine::digestToHex(md5.digest());
-        fm[key]=make_pair(tempname,name);
-        return make_pair(key,tempname);
-    }
+    std::shared_ptr<UploadedFile> add(const std::string& fileName, const std::string& mimeType);
     
-    std::pair<std::string,std::string> get(const std::string& param)
-    {
-        std::lock_guard<std::mutex> l(m);
-        return fm[param];
-    }
+    std::shared_ptr<UploadedFile> get(const std::string& linkKey);
+    
+    static const unsigned int maxFiles=10;
         
 private:
     FileMap();
     
-    std::map<std::string, std::pair<std::string,std::string>> fm;
+    void fileGarbageCollector();
+    
+    std::map<std::string, std::shared_ptr<UploadedFile>> fm;
+    std::list<std::shared_ptr<UploadedFile>> filesInDeleteOrder;
     std::mutex m;
+    std::condition_variable c;
 };
 
 #endif //FILE_H
