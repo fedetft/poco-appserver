fa2a7ed837c1e0c1f346cd96c5b740ba9bf994a3
Terraneo Federico <fede.tft@miosix.org>
2016-02-01 22:33:23 +0100

    Trying with bootstrap. Still broken

--- resources/download.sh
+++ resources/download.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-wget http://code.jquery.com/jquery-1.7.min.js
+#wget http://code.jquery.com/jquery-1.7.min.js
+wget code.jquery.com/jquery-1.11.0.min.js
 wget http://malsup.github.com/jquery.form.js
+wget https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css
+wget https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js
