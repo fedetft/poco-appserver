2d00abc31fbadc0cc867e74cdcfd906711feb958
Terraneo Federico <fede.tft@miosix.org>
2016-02-02 09:30:01 +0100

    Merged inline and out of line js

--- resources/uploadfile.js
+++ resources/uploadfile.js
@@ -19,3 +19,40 @@ $(document).ready( function() {
         
     });
 });
+
+(function() {
+var progress = $('.progress');
+var bar = $('.progress-bar');
+var percent = $('.percent');
+var status = $('#status');
+   
+$('form').ajaxForm({
+    beforeSend: function() {
+        status.empty();
+        status.hide();
+        progress.show();
+        var percentVal = '0%';
+        bar.width(percentVal);
+        percent.html(percentVal);
+    },
+    uploadProgress: function(event, position, total, percentComplete) {
+        var percentVal = percentComplete + '%';
+        bar.width(percentVal);
+        percent.html(percentVal);
+    },
+    success: function() {
+        var percentVal = '100%';
+        bar.width(percentVal);
+        percent.html(percentVal);
+    },
+    complete: function(xhr) {
+        progress.hide();
+        // Reset to fix animation
+        var percentVal = '0%';
+        bar.width(percentVal);
+        percent.html(percentVal);
+        status.html(xhr.responseText);
+        status.show();
+    }
+});
+})();
