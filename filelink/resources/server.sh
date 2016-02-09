365fb276f1154c0fa2e05afa4f40c7111cdba132
Terraneo Federico <fede.tft@miosix.org>
2016-02-09 23:58:00 +0100

    Moved filelink into its own separate repository, use git submodule to get it

--- filelink/resources/server.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Use python to start a web server and deliver the following static resources
-# to the web application. For a production environment, consider using a
-# real web server such as nginx
-python -m SimpleHTTPServer 9990
